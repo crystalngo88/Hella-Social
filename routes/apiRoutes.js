ff2d245fd22987986be3dae4c6c321c99a8043ae
PeterMacDonnell <petermacdonnell@berkeley.edu>
2018-09-14 16:00:50 -0700

    original backend setup

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,8 +9,8 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/user", function(req, res) {
+    db.User.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
