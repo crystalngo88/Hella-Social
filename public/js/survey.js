ecf5459ab822d9482c06002cb97699773ccc7306
Counttalkula666 <elenabelgum@gmail.com>
2018-09-18 15:38:16 -0700

    images alligned and resized, text changed

--- public/js/survey.js
+++ public/js/survey.js
@@ -5,22 +5,26 @@
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"> */
 
 // init the state from the input
-console.log("this works")
+console.log("this works");
 
-$(".image-checkbox").each(function () {
-    if ($(this).find('input[type="checkbox"]').first().attr("checked")) {
-      $(this).addClass('image-checkbox-checked');
-    }
-    else {
-      $(this).removeClass('image-checkbox-checked');
-    }
-  });
-  
-  // sync the state to the input
-  $(".image-checkbox").on("click", function (e) {
-    $(this).toggleClass('image-checkbox-checked');
-    var $checkbox = $(this).find('input[type="checkbox"]');
-    $checkbox.prop("checked",!$checkbox.prop("checked"));
-  
-    e.preventDefault();
-  });
+$(".image-checkbox").each(function() {
+  if (
+    $(this)
+      .find('input[type="checkbox"]')
+      .first()
+      .attr("checked")
+  ) {
+    $(this).addClass("image-checkbox-checked");
+  } else {
+    $(this).removeClass("image-checkbox-checked");
+  }
+});
+
+// sync the state to the input
+$(".image-checkbox").on("click", function(e) {
+  $(this).toggleClass("image-checkbox-checked");
+  var $checkbox = $(this).find('input[type="checkbox"]');
+  $checkbox.prop("checked", !$checkbox.prop("checked"));
+
+  e.preventDefault();
+});
