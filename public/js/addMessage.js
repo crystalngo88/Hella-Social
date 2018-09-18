fb64f0567e3e8394a8f8a101967582df02062060
Crystal Ngo <crystalngo88@gmail.com>
2018-09-17 22:31:14 -0700

    basic code for messageboard

---
+++ public/js/addMessage.js
@@ -0,0 +1,58 @@
+/* global moment */
+
+// When user clicks add-btn
+$("#message-submit").on("click", function(event) {
+    event.preventDefault();
+  
+    // Make a newMessage object
+    var newMessage = {
+      user: $("#user").val().trim(),
+      body: $("#message-box").val().trim(),
+      created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+    };
+  
+    console.log(newMessage);
+  
+    // Send an AJAX POST-request with jQuery
+    $.post("/api/new", newMessage)
+      // On success, run the following code
+      .then(function() {
+  
+        var row = $("<div>");
+        row.addClass("message");
+  
+        row.append("<p>" + newMessage.user + ": </p>");
+        row.append("<p>" + newMessage.body + "</p>");
+        row.append("<p>At " + moment(newMessage.created_at).format("h:mma on dddd") + "</p>");
+  
+        $("#message-area").prepend(row);
+  
+      });
+  
+    // Empty each input box by replacing the value with an empty string
+    $("#user").val("");
+    $("#message-box").val("");
+  });
+  
+  // When the page loads, grab all of our messages
+  $.get("/api/all", function(data) {
+  
+    if (data.length !== 0) {
+  
+      for (var i = 0; i < data.length; i++) {
+  
+        var row = $("<div>");
+        row.addClass("message");
+  
+        row.append("<p>" + data[i].user + " messaged.. </p>");
+        row.append("<p>" + data[i].body + "</p>");
+        row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+  
+        $("#message-area").prepend(row);
+  
+      }
+  
+    }
+  
+  });
+  
---
+++ routes/messageboardAPIRoutes.js
@@ -0,0 +1,45 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var addMessage = require("../public/js/addMessage.js");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Get all chirps
+  app.get("/api/all", function(req, res) {
+
+    // Finding all Chirps, and then returning them to the user as JSON.
+    // Sequelize queries are asynchronous, which helps with perceived speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    Message.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+
+  });
+
+  // Add a chirp
+  app.post("/api/new", function(req, res) {
+
+    console.log("Message Data:");
+    console.log(req.body);
+
+    Message.create({
+      user: req.body.user,
+      body: req.body.body,
+      created_at: req.body.created_at
+    }).then(function(results) {
+      // `results` here would be the newly created chirp
+      res.end();
+    });
+
+  });
+
+};
